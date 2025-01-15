3c83fa2b3d73c159ad3e2517e3f1929ebb62ee89
V1laZ <pasikstudiocz@gmail.com>
2025-01-15 18:12:03 +0100

    add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,36 @@
+import Aura from '@primevue/themes/aura'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@primevue/nuxt-module',
+  ],
+
   devtools: { enabled: true },
+
   compatibilityDate: '2024-11-01',
+
   eslint: {
     config: {
       stylistic: true,
     },
+  },
+
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primevue',
+            order: 'tailwind-base, primevue, tailwind-utilities',
+          },
+          darkModeSelector: '.dark',
+        },
+      },
+    },
+
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-primeui'),
+  ],
 }
